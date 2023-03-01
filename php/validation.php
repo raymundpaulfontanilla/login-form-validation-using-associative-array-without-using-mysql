6e7f959cfe8baa7ca3cf576147a779adf3771314
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-03-02 04:06:09 +0800

    removed all files and moved it to config folder

--- php/validation.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $confirmPassword = $_POST['confirm-password'];
-
-    $userData = array()
-?>
