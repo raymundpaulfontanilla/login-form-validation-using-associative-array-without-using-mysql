63c31e3b06eadd0d8536f4f5fe27b26a89edb8f1
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-03-02 04:04:05 +0800

    move php folder to config folder

---
+++ config/php/validation.php
@@ -0,0 +1,8 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirm-password'];
+
+    $userData = array()
+?>
