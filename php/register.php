1b5590888575b2013fb7637f69ec6dae184b6400
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-03-02 07:59:53 +0800

    added required attribute

--- php/register.php
+++ php/register.php
@@ -9,26 +9,26 @@
 </head>
 <body>
     <div class="container min-vh-100 d-flex justify-content-center align-items-center">
-        <form class="shadow-lg p-5 m-5 bg-body-tertiary rounded" method="POST" action="../config/php/register-user.php">
-            <div class="mb-3">
-                <label for="name" class="form-label">Name</label>
-                <input type="text" class="form-control" id="name" name="name">
-              </div>
-            <div class="mb-3">
-              <label for="email" class="form-label">Email</label>
-              <input type="email" class="form-control" id="email" name="email">
+      <form class="shadow-lg p-5 m-5 bg-body-tertiary rounded" method="POST" action="../config/php/register-user.php">
+          <div class="mb-3">
+              <label for="name" class="form-label">Name</label>
+              <input type="text" class="form-control" id="name" name="name" required>
             </div>
-            <div class="mb-3">
-              <label for="password" class="form-label">Password</label>
-              <input type="password" class="form-control" id="password" name="password">
+          <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email" name="email" required>
+          </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+          </div>
+          <div class="mb-3">
+              <label for="confirmPassword" class="form-label">Confirm Password</label>
+              <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" required>
             </div>
-            <div class="mb-3">
-                <label for="confirmPassword" class="form-label">Confirm Password</label>
-                <input type="password" class="form-control" id="confirmPassword" name="confirmPassword">
-              </div>
-            <button type="submit" class="btn btn-primary">Register</button>
-            <a href="login.php"><span>Already have an account?</span></a>
-          </form>
+          <button type="submit" class="btn btn-primary">Register</button>
+          <a href="login.php"><span>Already have an account?</span></a>
+        </form>
     </div>
 </body>
 </html>
