158d4a2845463e5f5dec592d4a0a42542ea3981f
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-03-08 20:38:49 +0800

    changed key variable into key string

--- validation/register-user.php
+++ validation/register-user.php
@@ -5,13 +5,18 @@
     $password = $_POST['password'];
     $confirmPassword = $_POST['confirmPassword'];
 
-    $_SESSION['userData'] = array($name => $name, $email => $email, $password => $password, $confirmPassword => $confirmPassword);
-
+    $_SESSION['userData'] = array(
+        "name" => $name, 
+        "email" => $email, 
+        "password" => $password, 
+        "confirmPassword" => $confirmPassword
+    );
+    
     if ($_SESSION['userData'][$password] != $_SESSION['userData'][$confirmPassword]) {
         $_SESSION['errorMessage'] = 'Password do not match!';
         header('location: ../php/register.php');
     } else {
         $_SESSION['successMessage'] = "Account successfully created";
-       header('location: ../php/login.php');
+       header('location: ../php/index.php');
     }
 ?>
